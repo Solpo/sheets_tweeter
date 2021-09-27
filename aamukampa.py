134d6c935fbc88a180f00b89823eb324e55873be
Solpo <3440817+Solpo@users.noreply.github.com>
2021-09-27 23:27:30 +0300

    kaupungin aamut

--- aamukampa.py
+++ aamukampa.py
@@ -8,8 +8,11 @@ taulukko = sheetin_luku.anna_taulukko("service_worker.json", sheet_id)
 
 tyoaamuja = sheetin_luku.lue(taulukko, "Sheet1", "A2")
 aamuja_kaikkiaan = sheetin_luku.lue(taulukko, "Sheet1", "C2")
+hese_aamuja = sheetin_luku.lue(taulukko, "Sheet1", "E2")
 
 if int(tyoaamuja) < 0 or int(aamuja_kaikkiaan) < 0:
     pass
 else:
-    twiittaus.twiittaa(api, f"Työaamuja vielä jäljellä {tyoaamuja}\nAamuja kaikkiaan jäljellä {aamuja_kaikkiaan}")
+    twiittaus.twiittaa(api, f"Työaamuja vielä jäljellä {tyoaamuja}\n"\
+        f"Aamuja kaikkiaan jäljellä {aamuja_kaikkiaan}\n"\
+            f"Aamuja tässä kaupungissa {hese_aamuja}")
